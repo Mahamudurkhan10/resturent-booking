0d31e96c7fa224879a468a92298c6bce7698ba97
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-10-17 01:42:43 +0600

    Book a table page done and foods category button added

--- src/pages/main/BookTable.jsx
+++ src/pages/main/BookTable.jsx
@@ -1,20 +1,49 @@
+import { MapPin, TableOfContents, User2Icon, Users } from 'lucide-react';
 import React from 'react';
 
 const BookTable = () => {
   return (
-    <div>
-       <div>
-         <div>
-            <img src="https://i.ibb.co.com/mG3qXGS/villagio1.jpg" alt="" />
-            <div>
-              <h1> Villagio Restaurant & Bar</h1>
+    <div className='pt-28 mb-12'>
+      <div className="  bg-white rounded-lg ">
+        <div className="space-y-4">
+          <img
+            src="https://i.ibb.co/mG3qXGS/villagio1.jpg"
+            alt="Villagio Restaurant"
+            className="w-full  object-cover rounded-md"
+          />
+
+          <h1 className="text-2xl font-bold text-gray-800"> <span className='text-2xl font-bold text-primary'>Villagio </span> Restaurant & Bar</h1>
+          <div className="w-full h-px bg-gray"></div>
+
+          <div className='flex gap-6'>
+            <div className="flex flex-col space-y-4">
+              <div className="flex items-center text-gray-600">
+                <MapPin className="text-primary mr-2" />
+                <span>360 San Lorenzo Avenue, Suite 1430 Coral Gables, FL 33146-1865</span>
+              </div>
+
+              <h2 className="text-lg font-semibold text-gray-700">Book a Table</h2>
+              <form className="flex flex-col sm:flex-row sm:space-x-2 gap-3 space-y-2 sm:space-y-0">
+                <input type="date" className="p-2 rounded-xl border-2 border-primary text-gray-700" />
+                <input type="time" className="p-2 rounded-xl border-2 border-primary text-gray-700" />
+
+                <div className='flex items-center gap-2'>
+                  <Users></Users>
+                  <input type="number" placeholder="Persons" min="1" className="p-2 rounded-xl border-2 border-primary text-gray-700" />
+                </div>
+              </form>
             </div>
-            <div className='w-full h-px bg-gray mt-2'></div>
-            <div>
-              
+            <div className='flex gap-2'>
+              <h1> <TableOfContents className='text-primary'></TableOfContents> </h1>
+              <p className='pt-2'>Villagio restaurant and bar has one mission: to provide guests with a fine and fresh seafood experience. Featuring seasonal and sustainable seafood that is flown in fresh daily, our chef-driven menu proves that no matter when you’re dining, seafood can be truly exceptional. to provide guests with a fine  <span className='font-semibold text-primary'>Read More...</span></p>
             </div>
-         </div>
-       </div>
+
+          </div>
+          <div className='flex justify-center pt-3 '>
+            <button className='btn  w-2/3  btn-outline text-xl font-semibold text-primary '> Book Now </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/main/Foods.jsx
+++ src/pages/main/Foods.jsx
@@ -4,32 +4,53 @@ import ExploreHeading from '../../components/shared/ExploreHeading';
 import { Search } from 'lucide-react';
 
 export default function Foods() {
-  const [foods,setFoods] = useState([])
+  const [foods, setFoods] = useState([])
+  const [selectedPlace, setSelectedPlace] = useState('');
 
-  useEffect(()=>{
-     const getFoods = async () =>{
-          const res = await fetch("./fack.json");
-          const data = await res.json();
+  useEffect(() => {
+    const getFoods = async () => {
+      const res = await fetch("./fack.json");
+      const data = await res.json();
+     
+      setFoods(data.foods)
+    }
+    getFoods();
+  }, [])
+   
+  const hanldeChange = (e) => {
+    e.preventDefault();
+    setSelectedPlace(e.target.value);
 
-          setFoods(data.foods)
-     }
-     getFoods();
-  },[])
+  };
 
   return (
-    <div className='pt-32 mb-12'>
+    <div className='pt-20 mb-12'>
+      <div >
+        <div className="p-4 w-1/2 pb-12 mx-auto lg:col-span-2 xl:flex grid grid-cols-4  gap-3 ">
+          {["", "Pizza", "Rice", "Kabab", "Rice", "Sushi", "Chicken"].map((place) => (
+            <button
+              key={place}
+              onClick={hanldeChange}
+              value={place}
+              className={` ${selectedPlace === place ? 'btn btn-warning' : 'btn text-white bg-primary btn-outline'}`}
+            >
+              {place || "ALL"}
+            </button>
+          ))}
+        </div>
+      </div>
       <div className='flex items-center justify-between gap-14  '>
-        <ExploreHeading /> 
+        <ExploreHeading />
         <div className='border flex items-center w-1/3 p-1 rounded-full'>
-                <input type="text" className='outline-none px-2 w-full py-1 bg-transparent' name="" id="" placeholder='Search Food' />
-                <button className='bg-primary text-white w-10 h-10 flex items-center justify-center rounded-full'><Search /></button>
-            </div>
+          <input type="text" className='outline-none px-2 w-full py-1 bg-transparent' name="" id="" placeholder='Search Food' />
+          <button className='bg-primary text-white w-10 h-10 flex items-center justify-center rounded-full'><Search /></button>
+        </div>
       </div>
       <div className='grid grid-cols-4 gap-4 mt-10'>
-            {
-                foods.map((item,index)=> <FoodCard key={item.id} item={item}  />)
-            }
-        </div>
+        {
+          foods.map((item, index) => <FoodCard key={item.id} item={item} />)
+        }
+      </div>
     </div>
   )
 }
